f89ccf14ad28bcf900d5c4facd7238bafc66e20a
vasile.vlasa <vlasq91@gmail.com>
2023-08-30 16:07:25 +0300

    Deleted the deleteDates helper and started writing it from 0 as the bug with deleting the dates on a reservation was not yet solved

--- src/helpers/deleteDates.ts
+++ src/helpers/deleteDates.ts
@@ -10,40 +10,19 @@ export default async function deleteDates(
   rooms: string[],
   customerId: number
 ) {
-  console.log('Rooms:', rooms);
-  let currentDate = new Date(entryDate);
-  let docSnap = await getDoc(doc(db, `${location}${userID}${currentDate.getFullYear()}`, `${currentDate.getMonth() + 1}`));
-  let data = docSnap.data();
-  console.log('First data:', data);
+  console.log(customerId);
+  const currentDate = new Date(entryDate);
+  const docRef = doc(db, `${location}${userID}${currentDate.getFullYear()}`, `${currentDate.getMonth() + 1}`);
+  const docSnap = await getDoc(docRef);
+  const data = docSnap.data();
   for (const room of rooms) {
-    const dateOfLeave = new Date(leaveDate);
-    let previousMonth = new Date(entryDate);
-    currentDate = new Date(entryDate);
-
-    while (currentDate <= dateOfLeave) {
-      if (currentDate.getMonth() !== previousMonth.getMonth()) {
-        await setDoc(doc(db, `${location}${userID}${previousMonth.getFullYear()}`, `${previousMonth.getMonth() + 1}`), data);
-        docSnap = await getDoc(doc(db, `${location}${userID}${currentDate.getFullYear()}`, `${currentDate.getMonth() + 1}`));
-        data = docSnap.data();
-        previousMonth.setFullYear(currentDate.getFullYear());
-        previousMonth.setMonth(currentDate.getMonth());
-      }
-      if (data) {
-        const day: string = data[room][currentDate.getDate()];
-        if (day && day.includes(`${customerId}`)) {
-          if (day.includes(`/`)) {
-            day.indexOf(`${customerId}`) < day.indexOf('/')
-              ? (data[room][currentDate.getDate()] = day.slice(0, day.indexOf('/')))
-              : (data[room][currentDate.getDate()] = day.slice(day.indexOf('/') + 1));
-          } else {
-            delete data[room][currentDate.getDate()];
-          }
+    if (data) {
+      const roomData = data[room];
+      for (const occupiedDay in roomData) {
+        if (roomData[occupiedDay].includes(customerId.toString())) {
+          console.log(roomData[occupiedDay]);
         }
-        console.log(room, day);
       }
-      currentDate.setDate(currentDate.getDate() + 1);
     }
-    await setDoc(doc(db, `${location}${userID}${currentDate.getFullYear()}`, `${currentDate.getMonth() + 1}`), data);
-    console.log(data);
   }
 }
